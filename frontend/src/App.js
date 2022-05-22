01749a3446de118934a0874e1dae33182e38761d
hamitmizrak <hamitmizrak@gmail.com>
2022-05-22 13:29:18 +0300

    React

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
+
+
+//rfc ==> TAB
+function App() {
+  return (
+<>
+<HeaderComponent/>
+<FooterComponent/>
+</>
+  );
+}
+
+export default App;
