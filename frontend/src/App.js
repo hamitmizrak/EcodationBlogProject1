b96c36449702ea4e2de9d7e0747707bff85cae75
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-05-29 11:24:27 +0300

    Router > Switch > Route.path.component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,31 @@
+import { BrowserRouter as  Router , Route , Switch } from 'react-router-dom'
+// import { Route } from 'react-router-dom'
+// import { Switch } from 'react-router-dom'
+// import { Router } from 'react-router-dom'
+
 import './App.css'
+import CreateAdminComponent from './components/CreateAdminComponent'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
+import ListAdminComponent from './components/ListAdminComponent'
+import ViewAdminComponent from './components/ViewAdminComponent'
 
 //rfc ==> TAB
 function App() {
   return (
     <>
-     
+     <Router>
         <HeaderComponent />
-
-      
-
+          <div className='container'>
+            <Switch>
+              <Route path="/" exact component={ListAdminComponent}></Route>
+              <Route path="/admins" component={ListAdminComponent}></Route>
+              <Route path="/add-admin/:id" component={CreateAdminComponent}></Route>
+              <Route path="/view-admin/:id" component={ViewAdminComponent}></Route>
+            </Switch>
+          </div>
         <FooterComponent />
-     
+        </Router>
     </>
   )
 }
